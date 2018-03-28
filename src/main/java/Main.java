2b4771b7d836e9ebdc5e56bd9d4b2bf5ff34344f
prumin <kostaki3@yandex.ru>
2018-03-28 14:56:54 +0300

    fixed?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
-        get("/get", (req, res) -> "Hello World");
+        get("/", (req, res) -> "Hello World");
     }
 
     static int getHerokuAssignedPort() {
