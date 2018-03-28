244f5d6fe9533e4700c9fa41cc8817542b3837ce
prumin <kostaki3@yandex.ru>
2018-03-28 15:04:22 +0300

    It's wingit add .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
         get("/", (req, res) -> "Hello World");
+        get("/getuniver", (req, res) -> "University");
+        get("/getcash", (req, res) -> "Cash");
     }
 
     static int getHerokuAssignedPort() {
