2c4ec180301943e2f7c03c62bed44601d03d7ec1
prumin <kostaki3@yandex.ru>
2018-03-28 14:50:09 +0300

    fixed error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,16 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         get("/get", (req, res) -> "Hello World");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 
 }
